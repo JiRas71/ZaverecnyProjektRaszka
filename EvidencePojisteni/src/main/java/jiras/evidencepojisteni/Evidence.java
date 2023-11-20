a11ddb5f2e5d44cf757796fc3472615153a394c5
JiRas71 <jiri.raszka@icloud.com>
2023-11-20 03:19:07 +0100

    Aktualizace výpisu všech klientů a výpisu nalezeného klienta.

--- EvidencePojisteni/src/main/java/jiras/evidencepojisteni/Evidence.java
+++ EvidencePojisteni/src/main/java/jiras/evidencepojisteni/Evidence.java
@@ -28,29 +28,47 @@ public class Evidence {
         System.out.println("Registrace klienta");
         System.out.println("--------------------------------------------");
         
-        do {
+        do { // Zadání křestního jména nového klienta
         System.out.print("Zadej jmeno: ");
             jmeno = sc.nextLine().trim();
-        } while (jmeno.equals(""));
-
-        do {
+            if ((jmeno.length() < 3) || (jmeno.length() > 10) || (jmeno.contains(" "))) {
+                System.out.println("Zadej 3 - 10 znaků");
+                jmeno = "";
+            }
+        } while (jmeno.equals("")); // Validace zadaného jména klienta
+        
+        do { // Zadání příjmení nového klienta
         System.out.print("Zadej příjmení: ");
             prijmeni = sc.nextLine().trim();
-        } while (prijmeni.equals(""));
+            if ((prijmeni.length() < 3) || (prijmeni.length() > 15) || (prijmeni.contains(" "))) {
+                System.out.println("Zadej 3 - 15 znaků");
+                prijmeni = "";
+            }
+        } while (prijmeni.equals("")); // Validace zadaného příjmení klienta
         
-        do {
+        do { // Zadání větu nového klienta
         System.out.print("Zadej věk: ");
             vek = sc.nextLine().trim();
-        } while (vek.equals(""));
+            if ((vek.length() < 1) || (vek.length() > 2) || (vek.contains(" "))) {
+                System.out.println("Zadej 1-2 ciferné číslo");
+                vek = "";
+            }
+        } while (vek.equals("")); // Validace zadaného věku klienta
         
-        do {
+        
+        do { // Zadání telefonu nového klienta
         System.out.print("Zadej telefon: +420");
             telefon = sc.nextLine().trim();
-        } while (telefon.equals(""));
+            if ((telefon.length() < 9) || (telefon.length() > 9) || (telefon.contains(" "))) {
+                System.out.println("Zadej 9 čísel bez mezer");
+                telefon = "";
+            }
+        } while (telefon.equals("")); // Validace zadaného telefonu klienta
         
         System.out.println("--------------------------------------------");
         System.out.println("Data byla uložena.");
 
+        // Uloží nového klienta
         klienti.add(new Klient(jmeno, prijmeni, vek, telefon));
     }
     
@@ -67,23 +85,31 @@ public class Evidence {
         System.out.println("Vyhledání pojištěného");
         System.out.println("--------------------------------------------");
         
-        do {
+        do { // Zadání křestního jména hledaného klienta
         System.out.print("Zadejte jméno pojištěného: ");
             jmeno = sc.nextLine().trim();
-        } while (jmeno.equals(""));
+            if ((jmeno.length() <= 3) || (jmeno.length() >= 10) || (jmeno.contains(" "))) {
+                System.out.println("Zadej 3 - 10 znaků");
+                jmeno = "";
+            }
+        } while (jmeno.equals("")); // Validace hledaného jména
         
-        do {
+        do { // Zadání příjmení hledaného klienta
         System.out.print("Zadejte příjmení pojištěného: ");
             prijmeni = sc.nextLine().trim();
-        } while (prijmeni.equals(""));
+            if ((prijmeni.length() <= 3) || (prijmeni.length() >= 15) || (prijmeni.contains(" "))) {
+                System.out.println("Zadej 3 - 15 znaků");
+                prijmeni = "";
+            }
+        } while (prijmeni.equals("")); // Validace hledaného příjmení
         
         System.out.println("\nNalezeno: ");
         
-        for (Klient klient : klienti) {
+        for (Klient klient : klienti) { // Vyhledání klienta
             if ((jmeno.equals(klient.getJmeno())) && (prijmeni.equals(klient.getPrijmeni()))) {
                 nalezeno.add(klient);
             }
         }
-        return nalezeno;
+        return nalezeno; // Navrátí hledaného klienta
     }
 }
